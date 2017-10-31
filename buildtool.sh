69edf6ab18d06c3724366a2c82a7f3601decf9a2
HatsanK <hatsank@gmail.com>
2017-10-31 18:29:01 +0900

    0.1.0m1
    메인메뉴의 ui한글화를 적용하였습니다.
    이미지 한글화: 사토P(18siki at dcinside.com)

--- buildtool.sh
+++ buildtool.sh
@@ -10,17 +10,28 @@ echo "* Firefox Buildtool: Copy Files"
 
 cp    manifest.json                     $DES/
 
-cp -R images                            $DES/images
-cp -R css/custom.css                    $DES/css/custom.css
 
+cp -R images                            $DES/images
+cp -R css                               $DES/css
 
 set +v
 echo "* Firefox Buildtool: Creating package..."
 pushd $DES/ > /dev/null
 # zip ../krpatch.cinderella.girls.xpi -qr *
-# web-ext lint
-web-ext sign
-zsh -c 'autoload zmv; zmv "web-ext-artifacts/(.*)-(.*)-an+fx.xpi" "../sidem_krpatch-$2-an+fx.xpi"'
+
+# https://github.com/mozilla/web-ext/issues/793
+unset WEB_EXT_API_KEY
+unset WEB_EXT_API_SECRET
+
+web-ext lint -v
+
+# https://github.com/mozilla/web-ext/issues/793
+export WEB_EXT_API_KEY=$ENV_WEB_EXT_API_KEY
+export WEB_EXT_API_SECRET=$ENV_WEB_EXT_API_SECRET
+
+web-ext sign -v
+
+zsh -c 'autoload zmv; zmv "web-ext-artifacts/sidem-(*)-an+fx.xpi" "../sidem_krpatch-$1-an+fx.xpi"'
 
 popd > /dev/null
 
